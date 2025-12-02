68d75619cc196be2c5246f95860d0b2d471bba67
Parameshwaran <parameshwaran.p2024cse@sece.ac.in>
2025-12-02 21:17:37 +0530

    Created component using flex

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import Banner from "./Components/Banner";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Components/Banner.jsx
@@ -0,0 +1,12 @@
+function Banner() {
+  return (
+    <section className="banner">
+      <h2 className="banner-title">Learners Today, Leaders Tomorrow</h2>
+      <p className="banner-text">
+        With our continuous research and development, we provide you with an excellent Aptitude training.
+      </p>
+    </section>
+  );
+}
+
+export default Banner;
