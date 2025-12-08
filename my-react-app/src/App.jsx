5dc63c53f3edfbb93cb738ce94b733619fcca247
Parameshwaran <parameshwaran.p2024cse@sece.ac.in>
2025-12-08 11:12:52 +0530

    Initial commit for new project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import FlexWrapComponent from "./flex-wrap/flex-wrap-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import OurPromisesComponent from "./our-promises/our-promises";
+import PositionExampleComponent from "./position-example/position-example-component";
+import PostionComponent from "./positioning/position-component";
+import PropsPractice2 from "./props-practice-two/props-practice2-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
+import Routes from "./routes/routes";
+
+function App() {
+  return (
+    <div>
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      {/* <LeadersTomorrowComponent /> */}
+      {/* <SymbolOfExcellenceComponent /> */}
+      {/* <PostionComponent /> */}
+      {/* {<TrustedbyComponent /> } */}
+      {/* <FlexWrapComponent /> */}
+      {/* <OurPromisesComponent /> */}
+      {/* <PositionExampleComponent /> */}
+      {/* {<PropsPractice2/>} */}
+      <Routes></Routes>
+
+    </div>
+  );
+}
+
+export default App;
